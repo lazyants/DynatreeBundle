8d042e2e0b9488ffab634d5ea2d54f33dfd1b4d9
maximspivakovskycamao <smaxims@gmail.com>
2013-01-17 09:30:39 +0100

    first commit

---
+++ LazyantsDynatreeBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Maxim Spivakovsky <maxim@lazy-ants.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Lazyants\DynatreeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LazyantsDynatreeBundle extends Bundle
+{
+}
